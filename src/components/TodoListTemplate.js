ec8bb9740a59c4aad59df29480d388f7fdb0bf9d
witch-factory <soakdma37@gmail.com>
2021-02-11 22:43:28 +0900

    class component로

--- src/components/TodoListTemplate.js
+++ src/components/TodoListTemplate.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import './TodoListTemplate.css';
 
-const TodoListTemplate=({form, children})=>{
-    return(
-        <main className="todo-list-template">
-            <div className="title">
-                오늘 할 일
-            </div>
-            <section className="form-wrapper">
-                {form}
-            </section>
-            <section className='todos-wrapper'>
-                {children}
-            </section>
-        </main>
-    );
+class TodoListTemplate extends React.Component{
+    render(){
+        return(
+            <main className="todo-list-template">
+                <div className="title">
+                    오늘 할 일
+                </div>
+                <section className="form-wrapper">
+                    {this.props.form}
+                </section>
+                <section className='todos-wrapper'>
+                    {this.props.children}
+                </section>
+            </main>
+        );
+    }
 }
+
 export default TodoListTemplate;
